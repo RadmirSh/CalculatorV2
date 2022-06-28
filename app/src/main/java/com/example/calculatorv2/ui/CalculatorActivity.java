6e2c3711e8de074eef51b4ebca22990074a6b382
RadmirSh <veselday@gmail.com>
2022-06-28 15:38:42 +0500

    Обработка нажатия кнопок

--- app/src/main/java/com/example/calculatorv2/ui/CalculatorActivity.java
+++ app/src/main/java/com/example/calculatorv2/ui/CalculatorActivity.java
@@ -3,14 +3,22 @@ package com.example.calculatorv2.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.calculatorv2.R;
+import com.example.calculatorv2.model.CalculatorImpl;
+import com.example.calculatorv2.model.Operator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalculatorActivity extends AppCompatActivity implements CalculatorView {
     /*отображение, сбор информации*/
 
     private TextView resultTxt;
+
+    private CalculatorPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,56 @@ public class CalculatorActivity extends AppCompatActivity implements CalculatorV
         setContentView(R.layout.activity_calculator);
 
         resultTxt = findViewById(R.id.result);
+
+        presenter = new CalculatorPresenter(this, new CalculatorImpl());
+
+        /*MAP чтобы вытащить значения*/
+        Map<Integer, Integer> digits = new HashMap<>();
+        digits.put(R.id.key_1, 1);
+        digits.put(R.id.key_2, 2);
+        digits.put(R.id.key_3, 3);
+        digits.put(R.id.key_4, 4);
+        digits.put(R.id.key_5, 5);
+        digits.put(R.id.key_6, 6);
+        digits.put(R.id.key_7, 7);
+        digits.put(R.id.key_8, 8);
+        digits.put(R.id.key_9, 9);
+        digits.put(R.id.key_0, 0);
+
+        /*digitClickListener для обработки нажатия кнопок и передачи presenter'у*/
+        View.OnClickListener digitOnClickListener = view -> presenter.onDigitPressed(digits.get(view.getId()));
+
+        /*выставление для кнопок listener */
+        findViewById(R.id.key_1).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_2).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_3).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_4).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_5).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_6).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_7).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_8).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_9).setOnClickListener(digitOnClickListener);
+        findViewById(R.id.key_0).setOnClickListener(digitOnClickListener);
+
+        /*MAP чтобы вытащить значения операторов*/
+        Map<Integer, Operator> operators = new HashMap<>();
+        operators.put(R.id.key_addition, Operator.ADD);
+        operators.put(R.id.key_subtraction, Operator.SUB);
+        operators.put(R.id.composition, Operator.COMP);
+        operators.put(R.id.division, Operator.DIV);
+
+        /*digitClickListener для обработки нажатия кнопок и передачи presenter'у*/
+        View.OnClickListener operatorsOnClickListener = view -> presenter.onOperatorPressed(operators.get(view.getId()));
+
+        /*выставление для кнопок listener */
+        findViewById(R.id.key_addition).setOnClickListener(operatorsOnClickListener);
+        findViewById(R.id.key_subtraction).setOnClickListener(operatorsOnClickListener);
+        findViewById(R.id.composition).setOnClickListener(operatorsOnClickListener);
+        findViewById(R.id.division).setOnClickListener(operatorsOnClickListener);
+
+        /*digitClickListener для обработки нажатия кнопки "." и передачи presenter'у*/
+        findViewById(R.id.dot).setOnClickListener(view -> presenter.onDotPressed());
+
     }
 
     @Override
@@ -25,3 +83,4 @@ public class CalculatorActivity extends AppCompatActivity implements CalculatorV
         resultTxt.setText(result);
     }
 }
+
