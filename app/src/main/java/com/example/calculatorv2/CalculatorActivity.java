a7bc3d2d8ccd771f551a2f5e92ea2efcdefa7c2e
RadmirSh <veselday@gmail.com>
2022-06-28 13:08:05 +0500

    Отделена задача от Android

--- app/src/main/java/com/example/calculatorv2/CalculatorActivity.java
+++ app/src/main/java/com/example/calculatorv2/CalculatorActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CalculatorActivity extends AppCompatActivity {
-
+    /*отображение, сбор информации*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
